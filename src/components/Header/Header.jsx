30b53e9fcbb5f6552d8f74ef47e051ead7e899d7
dev-atulyadav <atulyadav031103@gmail.com>
2024-05-03 21:02:17 +0530

    section created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
+import { TbMessage2Bolt } from "react-icons/tb";
 
 const Header = () => {
   return (
-    <header className="border-b border-black p-4 flex justify-between items-center">
-      <Link className="text-xl font-bold">
+    <header className="w-full border-b border-black p-4 flex justify-between items-center fixed top-0">
+      <Link className="text-xl font-bold flex justify-center items-center gap-2 z-20">
+        <TbMessage2Bolt className="text-4xl"/>
         <h1>ChatHub.in</h1>
       </Link>
       <Nav />
--- src/components/Home/Section.jsx
+++ src/components/Home/Section.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import Bg from "../../assets/bg.svg";
+import { Link } from "react-router-dom";
 
 const Section = () => {
   return (
-    <section className="">
-      <article>
-        <main>
-          <h1></h1>
+    <section className="flex justify-center items-center flex-col sm:flex-row p-8 flex-wrap md:flex-nowrap border-b border-black mt-12 sm:mt-20">
+      <article className="p-6">
+        <main className="flex flex-col justify-center items-start">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold">
+            Welcome at ChatHub.in
+          </h1>
+          <span className="sm:text-xl font-semibold">
+            <p>Stay connected with everyone.</p>
+            <p>Safe,reliable,and fast...</p>
+          </span>
+          <Link
+            to="/login"
+            className="p-2 border border-slate-800 text-slate-800 hover:bg-gray-500 hover:text-white rounded-lg mt-2 font-semibold"
+          >
+            Get Started
+          </Link>
         </main>
       </article>
+      <img className="h-96 sm:h-[30rem]" src={Bg} alt="can't load" />
     </section>
   );
 };
